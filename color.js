4ff5968ea229f45daaadaa518d625c2e4c13e8e5
CodingFeature <121998353+MovieGoers@users.noreply.github.com>
2023-01-09 18:35:47 +0900

    jQuery 라이브러리 추가.

--- color.js
+++ color.js
@@ -7,25 +7,17 @@ var body = {
     }
 }
 function nightDayMode(){
-var links = document.querySelectorAll('a');
-var i = 0; 
 if(document.querySelector('body').style.backgroundColor === 'white'){
     body.setBgColor('black');
     body.setColor('white');
     document.querySelector('h1').style.borderColor='white';
     document.querySelector('#list').style.borderColor='white';
-    while(i<links.length){
-        links[i].style.color = 'white';
-        i++;
-    }
+    $('a').css('color', 'white');
 }else{
     body.setBgColor('white');
     body.setColor('black');
     document.querySelector('h1').style.borderColor='black';
     document.querySelector('#list').style.borderColor='black';
-    while(i<links.length){
-        links[i].style.color = 'black';
-        i++;
-    }
+    $('a').css('color', 'black');
 }
 }
